e2b079e26e34bf90ddc2af8ae562b6104f51338f
Yiğit Nebioğlu <yigit.nebioglu@hotmail.com>
2022-11-20 15:39:07 +0300

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "11c611f4ade927c3ec0216e5a92cac06";
+export const WEATHER_API_KEY = "YOUR API KEY HERE";
